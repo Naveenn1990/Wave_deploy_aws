8e824974a232cecdff117a05fe9f4fa5eaeeb7c8
akashg7 <akashg250804@gmail.com>
2025-04-14 10:20:30 +0530

    done

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -815,10 +815,7 @@ exports.getPartnerDetails = async (req, res) => {
     console.error("Get Partner Details Error:", error);
     res.status(500).json({ message: "Error fetching partner details" });
   }
-};
-
-
-
+}; 
 
 // Update Partner Status
 exports.updatePartnerStatus = async (req, res) => {
@@ -847,29 +844,7 @@ exports.updatePartnerStatus = async (req, res) => {
         message: "Partner not found" 
       });
     }
-
-    // Additional validations based on status
-    // if (status === "Approved") {
-    //   // if (!partner.isVerified === "") {
-    //   //   return res.status(400).json({
-    //   //     success: false,
-    //   //     message: "Cannot approve partner without KYC verification"
-    //   //   });
-    //   // }
-    //   if (!partner.profileCompleted) {
-    //     return res.status(400).json({
-    //       success: false,
-    //       message: "Cannot approve partner without completed profile",
-    //       currentStatus: {
-    //         profileCompleted: partner.profileCompleted,
-    //         kycVerified: partner.kycDetails?.isVerified
-    //       },
-    //       note: "Please ensure the partner has completed their profile using the profile completion API"
-    //     });
-    //   }
-    // }
-
-    // Update the partner status
+ 
     const updateData = {
       $set: {
         status: status,
@@ -881,8 +856,7 @@ exports.updatePartnerStatus = async (req, res) => {
       partnerId,
       updateData,
       { new: true, runValidators: true }
-    ) 
-
+    )  
     res.json({
       success: true,
       message: `Partner status updated to ${status} successfully`,
